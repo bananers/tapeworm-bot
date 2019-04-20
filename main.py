44c25192130706d9b10c03fb0efd34b7a8ce3d5f
jaxsax <shengweichow@gmail.com>
2019-04-20 23:14:13 +0800

    Fix code to run on AppEngine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tapeworm import create_app
 
 app = create_app()
-app.run(host='127.0.0.1', port=8080, debug=True)
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080, debug=True)
