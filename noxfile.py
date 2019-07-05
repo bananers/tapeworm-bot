c47f8a89e8687d1fbc535c2d1e45853db98ad7ca
jaxsax <jaxsax@users.noreply.github.com>
2019-07-05 14:17:38 +0800

    Use nox (#21)

---
+++ noxfile.py
@@ -0,0 +1,26 @@
+import os
+import nox
+
+ON_TRAVIS = os.environ.get("TRAVIS") == "true"
+
+if not ON_TRAVIS:
+    nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session
+def lint(session):
+    session.install("-r", "requirements/test.txt")
+    session.run("black", "--check", "tapeworm", "tests", "noxfile.py")
+    session.run("pylint", "tapeworm", "tests")
+
+
+@nox.session
+def tests(session):
+    session.install("-r", "requirements/test.txt")
+    session.run("pytest")
+
+
+@nox.session
+def watch(session):
+    session.install("-r", "requirements/dev.txt")
+    session.run("ptw", "--runner=pytest")
