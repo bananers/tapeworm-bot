944fb6fe3e52292a52fce4991e16a0dfdfc4c04f
jaxsax <shengweichow@gmail.com>
2019-07-19 16:57:31 +0800

    Move StackDriver log patching

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,15 @@
+import logging
+
 from requests_toolbelt.adapters import appengine as requests_toolbelt_appengine
+import google.cloud.logging
+
 
 # Use the App Engine Requests adapter. This makes sure that Requests uses
 # URLFetch.
 requests_toolbelt_appengine.monkeypatch()
+
+# https://cloud.google.com/appengine/docs/standard/python/how-requests-are-handled#Python_The_environment
+client = google.cloud.logging.Client()
+client.setup_logging()
+
+logging.info("Stackdriver logging patched")
