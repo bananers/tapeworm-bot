87c01dc2fb9a58c8d35abc62dc1a19a61620336e
jaxsax <shengweichow@gmail.com>
2019-07-21 20:24:15 +0800

    Add severity for stackdriver

--- tapeworm/__init__.py
+++ tapeworm/__init__.py
@@ -4,8 +4,27 @@ import flask_injector
 from injector import inject
 
 from flask import Flask, g
+from pythonjsonlogger import jsonlogger
 
 import tapeworm.providers as providers
+
+
+class StackDriverJsonFormatter(jsonlogger.JsonFormatter):
+    def process_log_record(self, log_record):
+        log_record["severity"] = log_record["levelname"]
+        del log_record["levelname"]
+        log_record["timestamp"] = log_record["created"]
+        return super(StackDriverJsonFormatter, self).process_log_record(log_record)
+
+
+def setup_logging(level=logging.INFO):
+    handler = logging.StreamHandler()
+    formatter = StackDriverJsonFormatter("(created) (name) (levelname) (message)")
+    handler.setFormatter(formatter)
+
+    root_logger = logging.getLogger()
+    root_logger.addHandler(handler)
+    root_logger.setLevel(level)
 
 
 def create_app(test_config=None):
@@ -22,7 +41,7 @@ def create_app(test_config=None):
         app.config.update(test_config)
 
     if not app.testing:
-        logging.basicConfig(level=logging.DEBUG)
+        setup_logging(logging.DEBUG)
 
     app.config.update(TG_URL="https://api.telegram.org/bot" + app.config["TOKEN"] + "/")
     try:
