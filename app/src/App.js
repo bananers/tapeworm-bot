19f2167786dd9b0eb61893ca79cd6dca62e8c95f
jaxsax <shengweichow@gmail.com>
2019-11-24 15:50:42 +0800

    Make logo a js import

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Links from './Links';
+import logo from './images/tapeworm-icon.png'
 import { Header, Container, Image } from 'semantic-ui-react'
 
 function App(props) {
@@ -9,7 +10,7 @@ function App(props) {
 
       <Container>
       <Header size='huge'>
-        <Image circular src='/images/tapeworm-icon.png'  size='massive' style={{marginTop: "40px"}}/>
+        <Image circular src={logo}  size='massive' style={{marginTop: "40px"}}/>
         <Header.Content style={{marginTop: "40px"}}>
           Links @ {props.url}
         </Header.Content>
